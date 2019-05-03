8991996ac316b5ac05f5bfbc3a338090d775f897
richardo2016 <richardo2016@gmail.com>
2019-05-03 13:53:28 +0800

    use `packages` to store versioned types package

--- build/bin.js
+++ build/bin.js
@@ -1,13 +1,27 @@
 const fs = require('fs')
 const path = require('path')
+
+const semver = require('semver')
+const mkdirp = require('@fibjs/mkdirp')
+
+const { mkVersionPkg } = require('./utils')
 const tsdeclare = require('./tsdeclare')
 
 let installedNodeModules = path.resolve(__dirname, '../../../')
 if (!installedNodeModules || !installedNodeModules.endsWith('node_modules'))
     installedNodeModules = null
 
+const fibjs_version = process.env.FIBJS_VERSION
+
+if (!fibjs_version)
+    throw `fibjs version required, set property value to environment variable \`FIBJS_VERSION\`.`
+
+if (!semver.valid(fibjs_version))
+    throw `fibjs version invalid, set property value to environment variable \`FIBJS_VERSION\`.`
+
 const fibjs_src = installedNodeModules || process.env.FIBJS_SRC
-const target = process.env.FIBJS_TYPES_TARGET || path.resolve(__dirname, '../DefinitelyTyped/declare/');
+const package_target = mkVersionPkg(fibjs_version)
+const target = path.resolve(package_target, 'declare');
 
 if (!fibjs_src || !fs.exists(fibjs_src))
     throw `fibjs source location ${fibjs_src} invalid, set property value to environment variable \`FIBJS_SRC\`.`
@@ -16,5 +30,8 @@ const idl_src = path.resolve(fibjs_src, './idl/zh-cn');
 
 if (!fs.exists(idl_src))
     throw `fibjs idl source location ${idl_src} invalid.`
+
+if (!fs.exists(target))
+    mkdirp(target)
 
 tsdeclare(fibjs_src, target);
---
+++ build/utils.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const path = require('path')
+
+const semver = require('semver')
+
+const ejs = require('ejs')
+
+const copy = require('@fibjs/copy')
+
+exports.mkVersionPkg = function (fibjs_version) {
+    const pkgsrc = path.resolve(__dirname, './tmpl/package')
+    const mono_version = [ semver.major(fibjs_version), semver.minor(fibjs_version), 'x' ].join('.')
+
+    const target = getVersionedPkgLocation(mono_version)
+
+    copy(pkgsrc, target)
+
+    ;[
+        '.gitignore',
+        '.npmignore',
+    ].forEach(filename => 
+        fs.copy(
+            path.resolve(pkgsrc, filename),
+            path.resolve(target, filename)
+        )
+    )
+
+    renderEjs(target, './package.json.ejs', {version: fibjs_version})
+    renderEjs(target, './README.md.ejs', {version: fibjs_version})
+
+    return target
+}
+
+function getVersionedPkgLocation (fibjs_version) {
+    return path.resolve(__dirname, `../packages/${fibjs_version}`)
+}
+
+function renderEjs (ejsFilebase, ejsSrcFilename, ctx) {
+    const src = path.resolve(ejsFilebase, ejsSrcFilename)
+    const compiler = ejs.compile(fs.readTextFile(src));
+    const pkgJson = compiler({
+        version: ctx.version
+    });
+
+    const ejsTargetFilename = ejsSrcFilename.replace('.ejs', '')
+    const target = path.resolve(ejsFilebase, ejsTargetFilename)
+    fs.writeTextFile(target, pkgJson);
+    fs.unlink(src)
+}
