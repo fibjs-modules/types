d460c31cb136b3e6b5a63878a5db0f28abd261c2
Ray Chan <richardo2016@gmail.com>
2020-06-01 02:06:04 +0800

    add types for fibjs 0.30.x.

---
+++ packages/0.30.x/declare/MongoID.d.ts
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *                                                                         *
+ *   This file was automatically generated with idlc.js                    *
+ *	 build info: 								   						   *
+ *   	- fibjs	: 0.31.0-dev                                               *
+ *   	- date	: May 28 2020 19:40:10                                     *
+ *                                                                         *
+ ***************************************************************************/
+
+/** 
+ * @author Richard <richardo2016@gmail.com>
+ *
+ */
+
+/// <reference path="_common.d.ts" />
+/// <reference path="object.d.ts" />
+
+
+
+
+
+/** module Or Internal Object */
+/**
+	* @brief mongodb 数据库数据唯一标识对象，用于存储传递 oid
+	* @detail 
+	*/
+
+declare class Class_MongoID extends Class__object {
+	
+	
+	
+} /** endof class */
+
+/** endof `module Or Internal Object` */
+
+
