8862e4f64b122ed796cf1216af87bd823e7847ce
richardo2016 <richardo2016@gmail.com>
2021-03-22 15:06:25 +0800

    update test to test self reference objects.

--- type-test/events.ts
+++ type-test/events.ts
@@ -5,3 +5,5 @@ import events = require('events');
 events.EventEmitter
 
 new events.EventEmitter()
+
+var evt = new events.EventEmitter.EventEmitter();
--- type-test/global.ts
+++ type-test/global.ts
@@ -4,3 +4,7 @@
 global.a = 1;
 global.b = 2;
 global.c = 3;
+
+var buf1 = new Buffer();
+
+var buf2 = new Buffer.Buffer();
