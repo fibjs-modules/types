84dccaa5e3a47ba2a5ad980f91da0955f3254948
richardo2016 <richardo2016@gmail.com>
2021-05-19 16:10:45 +0800

    feat: use cached local fibjs source for every committish.

--- build/gen_types.js
+++ build/gen_types.js
@@ -1,61 +1,81 @@
+const fs = require('fs');
 const path = require('path');
 const zip = require('zip');
 
 const { mkdirp, getHttpClient }  = require('./utils');
 
+function getCommittishLocal (fibjsCommitsh = 'dev') {
+    const url = `https://github.com/fibjs/fibjs/archive/${fibjsCommitsh}.zip`;
+    const versionFileUrl = `https://raw.githubusercontent.com/fibjs/fibjs/${fibjsCommitsh}/fibjs/include/version.h`
+    const targetDir = path.resolve(__dirname, `../tmp/${fibjsCommitsh}`);
+
+    return {
+        url,
+        targetDir,
+        versionFileUrl,
+    }
+}
+
+function getFibjsVersion (str) {
+    return str.match(/fibjs_version\[\] = "(.*)"/)[1];
+}
+
 function downloadFibjsZip (fibjsCommitsh = 'dev') {
-    const url = `https://github.com/fibjs/fibjs/archive/${fibjsCommitsh}.zip`
+    const { url, versionFileUrl, targetDir } = getCommittishLocal(fibjsCommitsh);
 
     const client = getHttpClient();
-    console.warn(`[downloadFibjsZip] start download git archive zip from ${url}`)
-    const zipResp = client.get(url);
-    console.notice('[downloadFibjsZip] downloaded git archive zip!')
-
-    const zipFile = zip.open(zipResp.readAll())
-    // const archiveRoot = `fibjs-${fibjsCommitsh}`;
-    const archiveRoot = zipFile.namelist()[0].split('/')[0];
-    console.notice(`[downloadFibjsZip] archiveRoot is ${archiveRoot}!`)
-
-    const targetDir = path.resolve(__dirname, `../tmp/${fibjsCommitsh}`);
-    mkdirp(targetDir);
 
     let fibjsVersion
+    if (!fs.exists(targetDir)) {
+        console.warn(`[downloadFibjsZip] start download git archive zip from ${url}`)
+        const zipResp = client.get(url);
+        console.notice('[downloadFibjsZip] downloaded git archive zip!')
 
-    const idlNames = zipFile
-        .infolist()
-        .filter(info => {
-            const filename = info.filename;
-            if (filename.toLowerCase().indexOf(`version.h`) > -1) {
-                const matched = zipFile.read(filename).toString().match(/fibjs_version\[\] = "(.*)"/)
-                fibjsVersion = matched[1];
-                return false;
-            }
+        const zipFile = zip.open(zipResp.readAll())
+        // const archiveRoot = `fibjs-${fibjsCommitsh}`;
+        const archiveRoot = zipFile.namelist()[0].split('/')[0];
+        console.notice(`[downloadFibjsZip] archiveRoot is ${archiveRoot}!`)
 
-            return (
-                filename.toLowerCase().indexOf(`${archiveRoot}/idl`) > -1
-                || filename.toLowerCase().indexOf(`${archiveRoot}/tools`) > -1
-            )
-        })
-        .map(info => {
-            const relpath = info.filename.replace(new RegExp(`^${archiveRoot}\/`), '');
-            const targetPath = path.resolve(targetDir, relpath)
-            mkdirp(path.dirname(targetPath));
-
-            const isDir = Number(info.file_size) == 0;
-            // console.notice(`[downloadFibjsZip] we would extract zip member ${info.filename}`);
-
-            if (isDir) {
-                mkdirp(targetDir)
-            } else if (info.filename) {
-                zipFile.extract(info.filename, targetPath);
-            }
-
-            return info.filename
-        })
+        const idlNames = zipFile
+            .infolist()
+            .filter(info => {
+                const filename = info.filename;
+                if (filename.toLowerCase().indexOf(`version.h`) > -1) {
+                    fibjsVersion = getFibjsVersion(zipFile.read(filename).toString())
+                    return false;
+                }
     
-    console.log('[downloadFibjsZip] we downloaed idl files', idlNames);
-    console.notice(`[downloadFibjsZip] targetDir is ${targetDir}`);
-    console.notice(`[downloadFibjsZip] fibjsVersion is ${fibjsVersion}`);
+                return (
+                    filename.toLowerCase().indexOf(`${archiveRoot}/idl`) > -1
+                    || filename.toLowerCase().indexOf(`${archiveRoot}/tools`) > -1
+                )
+            })
+            .map(info => {
+                const relpath = info.filename.replace(new RegExp(`^${archiveRoot}\/`), '');
+                const targetPath = path.resolve(targetDir, relpath)
+                mkdirp(path.dirname(targetPath));
+    
+                const isDir = Number(info.file_size) == 0;
+                // console.notice(`[downloadFibjsZip] we would extract zip member ${info.filename}`);
+    
+                if (isDir) {
+                    mkdirp(targetDir)
+                } else if (info.filename) {
+                    zipFile.extract(info.filename, targetPath);
+                }
+    
+                return info.filename
+            })
+        
+        console.log('[downloadFibjsZip] we downloaed idl files', idlNames);
+        console.notice(`[downloadFibjsZip] targetDir is ${targetDir}`);
+        console.notice(`[downloadFibjsZip] fibjsVersion is ${fibjsVersion}`);
+    } else {
+        const content = client.get(versionFileUrl).readAll().toString();
+        console.notice(`[downloadFibjsZip] content of ${versionFileUrl} downloaded!`);
+        fibjsVersion = getFibjsVersion(content);
+        console.notice(`[downloadFibjsZip] fibjsVersion is ${fibjsVersion}`);
+    }
 
     return {
         targetDir,
