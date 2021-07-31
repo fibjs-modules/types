018454970e29a3b14c16664256e631c2a1f09597
richardo2016 <richardo2016@gmail.com>
2021-08-01 03:25:59 +0800

    build: generate package 0.33.x

---
+++ packages/0.33.x/dts/module/constants.d.ts
@@ -0,0 +1,13 @@
+/// <reference path="../../dts/_import/index.d.ts" />
+/**
+ * @description 常用常量定义模块
+ * 
+ *  引用方法：
+ *  ```JavaScript
+ *  var constants = require('constants');
+ *  ```
+ *  
+ */
+declare module 'constants' {
+}
+
--- scripts/pub.sh
+++ scripts/pub.sh
@@ -8,6 +8,7 @@ cd $DIR/packages/0.29.x && npm publish --registry=https://registry.npmjs.org;
 cd $DIR/packages/0.30.x && npm publish --registry=https://registry.npmjs.org;
 cd $DIR/packages/0.31.x && npm publish --registry=https://registry.npmjs.org;
 cd $DIR/packages/0.32.x && npm publish --registry=https://registry.npmjs.org;
+cd $DIR/packages/0.33.x && npm publish --registry=https://registry.npmjs.org;
 
 cd $DIR;
 
